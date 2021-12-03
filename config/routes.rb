c0ada5f313c370909ad2ddfb5c934f1dde745ae7
William Gotterer <william.gotterer@gmail.com>
2021-12-03 09:05:19 -0500

    commit tree 83a6f095ffbf8df2dfc72a0385773c22cb7f062f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users 
+  get "/signup", to: "users#create"
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
